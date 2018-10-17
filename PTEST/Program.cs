000964c2027054598f9f6765f5b6124bce897d74
amagicw <amagicw@gmail.com>
2018-10-17 14:05:06 +0200

    finished

--- PTEST/Program.cs
+++ PTEST/Program.cs
@@ -10,6 +10,16 @@ namespace PTEST
     {
         static void Main(string[] args)
         {
+            Int32 a = Int32.Parse(Console.ReadLine());
+            Int32 b = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(Calculate(a, b));
+        }
+
+        public static int Calculate(int a, int b)
+        {
+            if (b > 200 || b < 0 || a < 0)
+                throw new NotImplementedException();
+            return a + b;
         }
     }
 }
