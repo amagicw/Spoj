54843f32fda305470ad739d8112d8c5be1edadec
amagicw <amagicw@gmail.com>
2018-10-23 11:47:12 +0200

    finished

---
+++ SUMA/SUMA/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SUMA
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = 0;
+            List<int> numbers = new List<int>();
+            try
+            {
+                do
+                {
+                    n = Int32.Parse(Console.ReadLine());
+                    numbers.Add(n);
+                } while (n != 0);
+            }
+            catch(Exception ex)
+            {
+                DisplaySUM(numbers);
+            }
+        }
+
+        public static void DisplaySUM(List<int> numbers)
+        {
+            int sum = 0;
+            for(int i=0; i< numbers.Count; i++)
+            {
+                sum += numbers[i];
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
